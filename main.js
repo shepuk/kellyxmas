43de7d4766573a0ffadd660ffa219d4de1f43a6f
Paul Shepherd <paulshepherd@Pauls-MacBook-Air.local>
2023-12-24 23:34:21 +0000

    initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+let show = false;
+console.log('heya')
+
+function showmodal() {
+show = true;
+if (show == true) {
+    console.log("Challenges complete!");
+    modal.style.height = "10000px";
+    modal.style.position = "absolute";
+  }
+}
